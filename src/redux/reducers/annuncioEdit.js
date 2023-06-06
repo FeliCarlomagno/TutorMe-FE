0cf7d5a0848e0399e3ea25dc66b49f3f6d036ea1
FeliCarlomagno <feliciano.carlomagno@libero.it>
2023-06-06 22:11:45 +0200

    Add reducer for AnnuncioEdit

---
+++ src/redux/reducers/annuncioEdit.js
@@ -0,0 +1,20 @@
+const initialState = {};
+
+const annuncioEditReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "GET_EDIT_ANNUNCIO":
+      return {
+        ...state,
+        annuncioEdit: action.payload,
+      };
+    case "SET_ANNUNCIO_EDIT":
+      return {
+        ...state,
+        annuncioEdit: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default annuncioEditReducer;
