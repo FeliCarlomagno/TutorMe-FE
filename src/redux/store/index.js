4665b4a9760e1a5d2cc1f30e08c59c964ad2ba53
FeliCarlomagno <feliciano.carlomagno@libero.it>
2023-11-22 16:19:27 +0100

    Added persistence logic
    Added application-wide persistence logic

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,10 +1,23 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import signupUserReducer from "../reducers/signupUserReducer";
 import teacherReducer from "../reducers/teacherReducer";
 import loginUserReducer from "../reducers/loginUser";
 import annuncioReducer from "../reducers/AnnunciReducer";
 import annuncioSelezionatoReducer from "../reducers/annuncioSelezionato";
 import annuncioEditReducer from "../reducers/annuncioEdit";
+import { encryptTransform } from "redux-persist-transform-encrypt";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  transforms: [
+    encryptTransform({
+      secretKey: process.env.REACT_APP_PERSIST_KEY,
+    }),
+  ],
+};
 
 const rootReducer = combineReducers({
   userSignUp: signupUserReducer,
@@ -15,8 +28,12 @@ const rootReducer = combineReducers({
   annuncioEdit: annuncioEditReducer,
 });
 
-const store = configureStore({
-  reducer: rootReducer,
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
 });
+
+export const persistor = persistStore(store);
 
 export default store;
